e95d109df464fe98c32fd2b01c818855d3b8c3fd
Daniel Clavijo Núñez <148864537+Daniel0611CN@users.noreply.github.com>
2024-03-20 20:53:38 +0100

    Testing methods AlturaAlumno

--- src/test/java/org/iesvdm/AlturaAlumnoTest.java
+++ src/test/java/org/iesvdm/AlturaAlumnoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -61,9 +62,34 @@ public class AlturaAlumnoTest {
 
         /*then : */
         assertTrue(array[0] == result[0]);
-        assertTrue(result[result.length-1] == altura);
+        assertTrue(result[pos] == altura);
 
         System.out.println("El array modificado es: " + Arrays.toString(result));
+
+    }
+
+    @Test void modificaAlturaNoValido (){
+
+        /*when : */
+        double[] array = {1.5, 2.3, 3.2, 5.1, 7.6};
+        double altura = 3.4;
+        int pos = 8;
+
+        if (pos>=0 && pos<= array.length){
+
+            /*do : */
+            double[] result = AlturaAlumno.modificaAltura(array, pos, altura);
+
+            /*then : */
+            assertTrue(array[0] == result[0]);
+            assertTrue(result[pos] == altura);
+
+            System.out.println("El array modificado es: " + Arrays.toString(result));
+
+        }
+        else {
+            System.out.println("El array no se ha podido modificar, la posición no es válida.");
+        }
 
     }
 
@@ -104,13 +130,30 @@ public class AlturaAlumnoTest {
     @Test void mostrar () {
 
         /*when : */
-
+        String[] arraynombre = {"pepe", "paco", "dani", "angel", "cesar"};
+        double[] arrayAltura = {1.2, 5.1, 8.6, 6.3, 10.2};
 
         /*do : */
 
+        /* Aqui no se pone nada, ya que al tener que comprobar
+        * unicamente si el array no está vacío, pues no hace falta.
+        * Ademas es porque el metodo es void, por lo que no podemos
+        * asignar un tipo de dato al array result. */
 
         /*then : */
 
+        /* Aqui se comprueba si el array tiene mas de 0 posiciones
+        * en ese caso el metodo funcionaria correctamente.
+        * En caso contrario, indicaria que lo esperado es que devuelva
+        * true, pero sin embargo, al ser menor que 0, devuelve false. */
+
+        assertTrue(arraynombre.length>0);
+
+        if (arraynombre.length == arrayAltura.length){
+            System.out.println("Ambos arrays tienen la misma longitud.");
+        }
+
+        System.out.println("Se han impreso los arrays.");
 
     }
 
